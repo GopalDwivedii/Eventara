8a9719a7a63b2574c28a149fe8512e3705a7e020
Tushar Khatri <hello@tusharkhatri.in>
2025-11-11 12:56:22 +0530

    Create demo EventsController | Write EventRequest dto

---
+++ src/main/java/com/eventara/ingestion/model/dto/EventResponse.java
@@ -0,0 +1,4 @@
+package com.eventara.ingestion.model.dto;
+
+public class EventResponse {
+}
---
+++ src/main/java/com/eventara/ingestion/model/entity/Event.java
@@ -0,0 +1,6 @@
+package com.eventara.ingestion.model.entity;
+
+import jakarta.xml.bind.annotation.XmlID;
+
+public class Event {
+}
